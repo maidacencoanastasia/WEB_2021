a4474894cf7a24b2f816a08e45a593b39b45649a
Maidacenco Anastasia <maidacenco100@gmail.com>
2021-02-11 09:54:32 +0200

    Add files via upload
    Classwork 
    ===========================    1    ============================
    
    Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert:
    
    
    1, если значение больше нуля,
    
    -1, если значение меньше нуля,
    
    0, если значение равно нулю.
    
    Error, если введено не число
    
    
    
    =============================    2   ============================
    
    Напишите условие if для проверки, что значение переменной age НЕ находится в диапазоне 14 и 90 включительно.
    
    Напишите два варианта: первый с использованием оператора НЕ !, второй – без этого оператора.
    
    
    =================================   3   ===============================
    Some issues with this part
    Напишите код который проверит правильность username и password и выводит следующие сообщения:
    
    Привет, если совпадает username и password
    
    Неправильный пароль если username введен правильно а пароль нет
    
    Неправильный username, если введен неправильный username
    
    Ошибка Валидации, если ничего не введено
    
    PS: На практике не делать такое никогда (писать пользователю что он ввел неверно, username или password)
    
    
    
    ================================   4   =================================
    Напишите код который посчитает и выведет в консоль сумму четных чисел из интервала [a, b]

---
+++ 11.02.2021/main.js
@@ -0,0 +1,46 @@
+ //////////////////////////////////////// 1 //////////////////////////////////////
+result = prompt("Introduceti un numar")
+if (result>0) {
+	alert(1);
+}else if (result<0){
+	alert(-1);
+}else if (result==0){
+	alert(0);
+}else if (isNaN(result)){
+	alert("Error");
+}
+////////////////////////////////////////// 2 ////////////////////////////////////
+age = 100;
+if (!(age >= 14 && age <= 90)){
+	alert("not in [14;90] first method");
+}
+if (age < 14 || age > 90){
+	alert("not in [14;90] second method")
+}
+///////////////////////////////////////// 3 ///////////////////////////////////////
+function myFunccc(){
+	let admin = "admin"
+	let pole = document.getElementById("pole");
+	let pass = document.getElementById("pass");
+	if ( pole.value ===" " ){
+    console.log("Ошибка Валидации");
+  }else if (pole.value == pass.value) {
+    console.log("Привет");
+  } else if (pole.value != admin){
+    console.log("Неправильный username");
+  }else if (pass.value != admin ){
+    console.log("Неправильный пароль");
+  }
+}
+
+////////////////////////////////////////  4  ///////////////////////////////////////
+function myFunction(){
+	let a = parseInt(document.getElementById("var1").value);
+	let b = parseInt(document.getElementById("var2").value);
+	for (let i = a; i <= b; i++) {
+	if (i%2==0) {
+		console.log(i)
+		}
+	}
+}
+
