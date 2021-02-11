9bb1ca130b7422e97dd9323d49217114007dcd13
Maidacenco Anastasia <maidacenco100@gmail.com>
2021-02-11 10:46:50 +0200

    Update main.js
    Problem solved

--- 11.02.2021/main.js
+++ 11.02.2021/main.js
@@ -1,45 +1,99 @@
- //////////////////////////////////////// 1 //////////////////////////////////////
+  //////////////////////////////////////// 1 //////////////////////////////////////
 result = prompt("Introduceti un numar")
-if (result>0) {
-	alert(1);
-}else if (result<0){
-	alert(-1);
-}else if (result==0){
-	alert(0);
-}else if (isNaN(result)){
-	alert("Error");
+if (result > 0) {
+    alert(1);
+} else if (result < 0) {
+    alert(-1);
+} else if (result == 0) {
+    alert(0);
+} else if (isNaN(result)) {
+    alert("Error");
 }
 ////////////////////////////////////////// 2 ////////////////////////////////////
-age = 100;
-if (!(age >= 14 && age <= 90)){
-	alert("not in [14;90] first method");
+let age = 100;
+if (!(age >= 14 && age <= 90)) {
+    alert(age+", age is not in [14;90] first method");
 }
-if (age < 14 || age > 90){
-	alert("not in [14;90] second method")
+if (age < 14 || age > 90) {
+    alert(age+", age is not in [14;90] second method")
 }
 ///////////////////////////////////////// 3 ///////////////////////////////////////
-function myFunccc(){
-	let admin = "admin"
-	let pole = document.getElementById("pole");
-	let pass = document.getElementById("pass");
-	if ( pole.value ==" "||pass.value =="" ){
-    console.log("Ошибка Валидации");
-  }else if (pole.value === pass.value) {
-    console.log("Привет");
-  } else if (pole.value != admin){
-    console.log("Неправильный username");
-  }else if (pass.value != admin ){
-    console.log("Неправильный пароль");
-  }
+const correct_username = "admin"
+const correct_password = "123"
+
+function password_check() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    if (username.value == "" || password.value == "") {
+        console.log("Ошибка Валидации");
+    } else if (username.value == password.value) {
+        console.log("Привет");
+    } else if (username.value != correct_username) {
+        console.log("Неправильный username");
+    } else if (password.value != correct_password) {
+        console.log("Неправильный пароль");
+    } else if (password.value == correct_password && password.value == correct_password) {
+        console.log("Все ок");
+    } else if (username.value != correct_username && password.value != correct_password) {
+        console.log("Все не правильно");
+    }
 }
 ////////////////////////////////////////  4  ///////////////////////////////////////
-function myFunction(){
-	let a = parseInt(document.getElementById("var1").value);
-	let b = parseInt(document.getElementById("var2").value);
-	for (let i = a; i <= b; i++) {
-	if (i%2==0) {
-		console.log(i)
-		}
-	}
+function myFunction() {
+    let start_interval = parseInt(document.getElementById("start_interval").value);
+    let end_interval = parseInt(document.getElementById("end_interval").value);
+    for (let i = start_interval; i <= end_interval; i++) {
+        if (i % 2 == 0) {
+            console.log(i)
+        }
+    }
+} //////////////////////////////////////// 1 //////////////////////////////////////
+result = prompt("Introduceti un numar")
+if (result > 0) {
+    alert(1);
+} else if (result < 0) {
+    alert(-1);
+} else if (result == 0) {
+    alert(0);
+} else if (isNaN(result)) {
+    alert("Error");
 }
+////////////////////////////////////////// 2 ////////////////////////////////////
+let age = 100;
+if (!(age >= 14 && age <= 90)) {
+    alert(age+", age is not in [14;90] first method");
+}
+if (age < 14 || age > 90) {
+    alert(age+", age is not in [14;90] second method")
+}
+///////////////////////////////////////// 3 ///////////////////////////////////////
+const correct_username = "admin"
+const correct_password = "123"
 
+function password_check() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    if (username.value == "" || password.value == "") {
+        console.log("Ошибка Валидации");
+    } else if (username.value == password.value) {
+        console.log("Привет");
+    } else if (username.value != correct_username) {
+        console.log("Неправильный username");
+    } else if (password.value != correct_password) {
+        console.log("Неправильный пароль");
+    } else if (password.value == correct_password && password.value == correct_password) {
+        console.log("Все ок");
+    } else if (username.value != correct_username && password.value != correct_password) {
+        console.log("Все не правильно");
+    }
+}
+////////////////////////////////////////  4  ///////////////////////////////////////
+function myFunction() {
+    let start_interval = parseInt(document.getElementById("start_interval").value);
+    let end_interval = parseInt(document.getElementById("end_interval").value);
+    for (let i = start_interval; i <= end_interval; i++) {
+        if (i % 2 == 0) {
+            console.log(i)
+        }
+    }
+}
